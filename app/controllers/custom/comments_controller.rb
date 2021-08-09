0c5ae5ef8fa9baa4f84a931460226bcdd77802d1
Alejandro <alejandro.vera1988@gmail.com>
2021-08-09 16:55:52 +0100

    Borro "ruby" del parcial y añado la acción update en el callback de authentication

--- app/controllers/custom/comments_controller.rb
+++ app/controllers/custom/comments_controller.rb
@@ -1,13 +1,12 @@
 require_dependency Rails.root.join('app', 'controllers', 'comments_controller').to_s
 
 class CommentsController
-  before_action :authenticate_user!, only: :create
+  before_action :authenticate_user!, only: [:create, :update]
   before_action :load_commentable, only: [:create, :update]
   before_action :verify_resident_for_commentable!, only: :create
   before_action :verify_comments_open!, only: [:create, :vote]
   before_action :build_comment, only: :create
   before_action :update_comment, only: :update
-
 
   def create
     if @comment.save
@@ -29,8 +28,8 @@ class CommentsController
 
   private
 
-  def update_comment
-    @comment = Comment.find(params[:id])
-    @comment.body = comment_params[:body]
-  end
+    def update_comment
+      @comment = Comment.find(params[:id])
+      @comment.body = comment_params[:body]
+    end
 end
