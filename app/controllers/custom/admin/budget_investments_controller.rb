5e073c74075b6163d32932a6055e72f391dd3a24
Adán Alonso Salvador <adan.alonso.s@gmail.com>
2022-08-11 09:04:29 +0100

    Syntax errors in permitted params
    Signed-off-by: Adán Alonso Salvador <adan.alonso.s@gmail.com>

--- app/controllers/custom/admin/budget_investments_controller.rb
+++ app/controllers/custom/admin/budget_investments_controller.rb
@@ -5,8 +5,9 @@ class Admin::BudgetInvestmentsController
     def budget_investment_params
       attributes = [:external_url, :heading_id, :administrator_id, :tag_list,
                     :valuation_tag_list, :incompatible, :visible_to_valuators, :selected,
-                    :milestone_tag_list, valuator_ids: [], valuator_group_ids: [],
-                    :organization_name, :location, :not_selected]
+                    :milestone_tag_list,
+                    :organization_name, :location, :not_selected,
+                    valuator_ids: [], valuator_group_ids: []]
       params.require(:budget_investment).permit(attributes, translation_params(Budget::Investment))
     end
 end
--- app/controllers/custom/admin/poll/polls_controller.rb
+++ app/controllers/custom/admin/poll/polls_controller.rb
@@ -4,8 +4,8 @@ class Admin::Poll::PollsController
 
     def poll_params
       attributes = [:name, :starts_at, :ends_at, :geozone_restricted, :budget_id, :related_sdg_list,
-                    geozone_ids: [], image_attributes: image_attributes,
-                    :starts_at_hour, :ends_at_hour]
+                    :starts_at_hour, :ends_at_hour,
+                    geozone_ids: [], image_attributes: image_attributes]
       params.require(:poll).permit(*attributes, *report_attributes, translation_params(Poll))
     end
 end
