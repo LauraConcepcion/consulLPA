f82ea9913d31a4c4a9c8fcbef3e406e4a89d6054
Adán Alonso Salvador <adan.alonso.s@gmail.com>
2022-09-05 16:22:57 +0100

    Update app/views/budgets for v1.4.1
    Signed-off-by: Adán Alonso Salvador <adan.alonso.s@gmail.com>

---
+++ app/components/custom/budgets/investments/form_component.rb
@@ -0,0 +1,4 @@
+class Budgets::Investments::FormComponent < ApplicationComponent; end
+require_dependency Rails.root.join("app", "components", "budgets", "investments", "form_component").to_s
+class Budgets::Investments::FormComponent
+end
