12b5ac7cddf3ed1038ac8bcbd1ec6966bfaff07f
Adán Alonso Salvador <adan.alonso.s@gmail.com>
2022-09-05 16:12:25 +0100

    Revert "Update app/views/budgets for v1.4.1"
    This reverts commit a802d2ce33ec994bd588c7784761653b67abc3ed.

--- app/components/custom/admin/budgets/form_component.rb
+++
@@ -1,4 +0,0 @@
-class Admin::Budgets::FormComponent < ApplicationComponent; end
-require_dependency Rails.root.join("app", "components", "admin", "budgets", "form_component").to_s
-class Admin::Budgets::FormComponent
-end
