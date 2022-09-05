5867430a1e15ec6875336bf973a9a72210bea985
Adán Alonso Salvador <adan.alonso.s@gmail.com>
2022-09-05 11:45:07 +0100

    Update app/views/admin for v1.4.1
    Signed-off-by: Adán Alonso Salvador <adan.alonso.s@gmail.com>

---
+++ app/components/custom/admin/budget_phases/form_component.rb
@@ -0,0 +1,4 @@
+class Admin::BudgetPhases::FormComponent < ApplicationComponent; end
+require_dependency Rails.root.join("app", "components", "admin", "budget_phases", "form_component").to_s
+class Admin::BudgetPhases::FormComponent
+end
