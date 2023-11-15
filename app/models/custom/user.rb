2977f567bcbbd0a3a9e06a94ddfe7e994ec21872
Alejandro <alejandro.vera1988@gmail.com>
2023-11-15 09:15:07 +0000

    Add verification of residence in user management

--- app/models/custom/user.rb
+++ app/models/custom/user.rb
@@ -1,21 +1,38 @@
 require_dependency Rails.root.join("app", "models", "user").to_s
 class User
+
+  def strf_date_of_birth
+    date_of_birth.strftime("%d/%m/%Y")
+  end
+
   def minimum_votation_required_age?
     return false if date_of_birth.blank?
 
     Age.in_years(date_of_birth) >= User.minimum_required_age
   end
 
-  def after_database_authentication
+  def verify_residence_verified_at
     if self.residence_verified_at && self.residence_verified_at < Time.current - 12.months
-      date_of_birth_fmt = date_of_birth.strftime("%d/%m/%Y")
+      date_of_birth_fmt = strf_date_of_birth
       r = CensusApi.new.call(document_type, document_number, date_of_birth_fmt)
       if r.valid?
         self.update_attribute(:residence_verified_at, Time.current)
       else
+        logger.error "Error de #{current_user.id} al verificarse"
+        logger.error r.errors
         self.update_attribute(:residence_verified_at, nil)
         self.update_attribute(:verified_at, nil)
       end
     end
   end
+
+  def after_database_authentication
+    verify_residence_verified_at
+  end
+
+  private
+
+    def logger
+      @logger ||= ApplicationLogger.new
+    end
 end
--- app/models/custom/verification/management/document.rb
+++ app/models/custom/verification/management/document.rb
@@ -1,8 +1,13 @@
 require_dependency Rails.root.join("app", "models", "verification", "management", "document").to_s
 class Verification::Management::Document
+
   def in_census?
     response = CensusCaller.new.call(document_type, document_number, date_of_birth)
     response.valid?
+  end
+
+  def residence_verified_at?
+    user.residence_verified_at
   end
 
   def valid_age?(response)
