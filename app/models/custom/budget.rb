91546e93edb6c045ac7d54f6248ac7bde18b4845
Alejandro <alejandro.vera1988@gmail.com>
2023-11-16 12:10:13 +0000

    Add automatic change of participatory budgeting phase

--- app/models/custom/budget.rb
+++ app/models/custom/budget.rb
@@ -1,5 +1,7 @@
 require_dependency Rails.root.join("app", "models", "budget").to_s
 class Budget
+  include Rails.application.routes.url_helpers
+
   def formatted_amount(amount)
     ActionController::Base.helpers.number_to_currency(amount,
                                                       precision: 2,
@@ -17,4 +19,34 @@ class Budget
       %w[random confidence_score]
     end
   end
+
+  def self.change_phase
+    self.change_active_phases
+  end
+
+  private
+
+    def self.active_bugets
+      Budget.all.reject{|budget| budget.finished?}
+    end
+
+    def self.change_active_phases
+      notifier = SlackNotifier.new
+      self.active_bugets.map do |budget|
+        previous_phase = budget.phase
+        next_phase = budget.published_phases.find do |phase|
+          phase.starts_at == Time.zone.now
+        end
+        if next_phase.present?
+          budget.phase = next_phase.kind
+          if budget.save
+            notifier.post text: I18n.t('general.slack.budgets.phase_change.success', budget_name: budget.name,
+            previous_phase: I18n.t("budgets.phase.#{previous_phase}"),
+            next_phase: I18n.t("budgets.phase.#{next_phase.kind}"))
+          else
+            notifier.post text: I18n.t('general.slack.budgets.phase_change.error')
+          end
+        end
+      end
+    end
 end
