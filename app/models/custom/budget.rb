f29dea55468b47ea5398bd1668834bedd5b83096
Alejandro <alejandro.vera1988@gmail.com>
2023-11-21 16:29:21 +0000

    Crea scope and compare day not time

--- app/models/custom/budget.rb
+++ app/models/custom/budget.rb
@@ -2,6 +2,8 @@ require_dependency Rails.root.join("app", "models", "budget").to_s
 class Budget
   include Rails.application.routes.url_helpers
 
+  scope :active_budgets, -> { reject(&:finished?) }
+
   def formatted_amount(amount)
     ActionController::Base.helpers.number_to_currency(amount,
                                                       precision: 2,
@@ -21,32 +23,28 @@ class Budget
   end
 
   def self.change_phase
-    self.change_active_phases
+    change_active_phases
   end
 
   private
 
-    def self.active_bugets
-      Budget.all.reject{|budget| budget.finished?}
-    end
-
     def self.change_active_phases
       notifier = SlackNotifier.new
-      self.active_bugets.map do |budget|
+      active_budgets.map do |budget|
         previous_phase = budget.phase
         next_phase = budget.published_phases.find do |phase|
-          phase.starts_at == Time.zone.now
+          I18n.l(phase.starts_at.to_date) == I18n.l(Time.zone.now.to_date)
         end
         if next_phase.present?
           budget.phase = next_phase.kind
           if budget.save
-            notifier.post text: I18n.t('general.slack.budgets.phase_change.success', budget_name: budget.name,
+            notifier.post text: I18n.t("general.slack.budgets.phase_change.success", budget_name: budget.name,
             previous_phase: I18n.t("budgets.phase.#{previous_phase}"),
             next_phase: I18n.t("budgets.phase.#{next_phase.kind}"))
           else
-            notifier.post text: I18n.t('general.slack.budgets.phase_change.error')
+            notifier.post text: I18n.t("general.slack.budgets.phase_change.error")
           end
         end
       end
-    end
+  end
 end
