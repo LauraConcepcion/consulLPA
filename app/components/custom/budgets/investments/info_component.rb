3b9c78015c5db0120ddf579254e70afebd64f4e5
Alejandro Vera <alejandro.vera1988@gmail.com>
2025-06-23 16:45:29 +0100

    feat: Add tag for the milestone of investment

---
+++ app/components/custom/budgets/investments/info_component.rb
@@ -0,0 +1,6 @@
+
+class Budgets::Investments::InfoComponent < ApplicationComponent; end
+require_dependency Rails.root.join("app", "components", "budgets", "investments", "info_component").to_s
+
+class Budgets::Investments::InfoComponent
+end
