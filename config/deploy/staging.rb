fe4d668bf7f4cb8981cdcacbabd36830afbe48e4
Alejandro Vera <avera@usabi.es>
2026-05-20 13:32:12 +0100

    chore: Habilitar RVM solo en despliegue a producción

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,3 +1,3 @@
-set :branch, ENV["branch"] || 'issue/8971'
+set :branch, ENV["branch"] || 'feat/banner-presupuestos-2026-2027'
 
 server deploysecret(:server), user: deploysecret(:user), roles: %w[web app db importer cron background]
