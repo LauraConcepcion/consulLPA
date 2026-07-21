9d6f8bee8dd7a6124a1bdcdaf25eb8b1cd71a1cf
Alejandro Vera <avera@usabi.es>
2026-07-21 14:27:05 +0100

    chore: Desplegar staging desde la rama devel
    devel pasa a ser la rama de preproducción.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,3 +1,3 @@
-set :branch, ENV["branch"] || 'feat/banner-presupuestos-2026-2027'
+set :branch, ENV["branch"] || :devel
 
 server deploysecret(:server), user: deploysecret(:user), roles: %w[web app db importer cron background]
